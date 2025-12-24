14dbfdf464ad14ad2f09a9dd0132c7b5aed58cd2
Alexandre Agasseau <53575713+YouKyi@users.noreply.github.com>
2025-12-24 19:36:11 +0100

    Change base URL for GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/traefik-archi/', // GitHub Pages base URL
+  base: '/', // GitHub Pages base URL
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
